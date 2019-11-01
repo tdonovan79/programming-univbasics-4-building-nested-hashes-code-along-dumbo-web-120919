e9397ecd53db6906025d9ef065522fe047dddc46
Thomas Donovan <tdonovan79@gmail.com>
2019-11-01 20:18:16 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,7 +10,8 @@ def third_challenge
         age: "54"
         },
       :hero => {
-        #your key/value pairs here
+        name: "Romeo",
+        
         },
       :hero_friends => []
    }, 
